11a0492d95225d21046b345d7266d705ed3fc022
Tact1234 <andliwake@yahoo.co.jp>
2021-05-15 20:38:03 +0900

    app.js/app.cssの参照方法を修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-				<script src="{{ asset('js/app.js') }}" defer></script>
-				<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+				@if(app('env')=='local')
+					<script src="{{ asset('js/app.js') }}" defer></script>
+					<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+				@endif
+				@if(app('env')=='production')
+					<script src="{{ secure_asset('js/app.js') }}" defer></script>
+					<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+				@endif
         <title>chatApp</title>
     </head>
     <body>
